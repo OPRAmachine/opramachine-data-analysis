28189adf48709fe91f05bdf86d7c1ba53ad1f19f
Gavin Rozzi <gr@gavinrozzi.com>
2020-02-17 01:19:10 -0500

    Update opra-analysis.py
    Simplify code that parses dates from CSV columns

--- opra-analysis.py
+++ opra-analysis.py
@@ -1,15 +1,9 @@
 import pandas as pd 
 
-# Read CSV of OPRA request data to Pandas dataframe
-opra_requests = pd.read_csv("opramachine_requests.csv")
+# Read CSV of OPRA request data to Pandas dataframe, parse dates
 
-# Convert date time columns to proper type
-opra_requests['request_created_at'] = opra_requests['request_created_at'].astype('datetime64[ns]') 
-opra_requests['request_updated_at'] = opra_requests['request_updated_at'].astype('datetime64[ns]') 
-opra_requests['date_initial_request_last_sent_at'] = opra_requests['date_initial_request_last_sent_at'].astype('datetime64[ns]') 
-opra_requests['date_response_required_by'] = opra_requests['date_response_required_by'].astype('datetime64[ns]') 
-opra_requests['date_very_overdue_after'] = opra_requests['date_very_overdue_after'].astype('datetime64[ns]') 
-opra_requests['last_public_response_at'] = opra_requests['last_public_response_at'].astype('datetime64[ns]') 
+opra_requests = pd.read_csv('https://raw.githubusercontent.com/gavinrozzi/opra-data/master/opramachine_requests.csv', encoding= 'unicode_escape', parse_dates=['request_created_at','request_updated_at','date_initial_request_last_sent_at','date_response_required_by','date_very_overdue_after','last_public_response_at'])
 
 # Calculate difference in time
 opra_requests['days_until_response'] = opra_requests['last_public_response_at'] - opra_requests['date_initial_request_last_sent_at']
+
